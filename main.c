b7aba037ccda71676475493e2825714a41a65495
nh0126 <nh0126@sookmyung.ac.kr>
2023-11-05 21:16:23 +0900

    실습2

--- main.c
+++ main.c
@@ -7,14 +7,20 @@
 
 int main(int argc, char *argv[]) {
 	
-	int grade [N_STUDENT];
-	int i;
+	int i, average;
+	int sum;
+	int grade[N_STUDENT];
+	printf("input 5 scores:");
 	
+	sum = 0;
 	for (i=0;i<N_STUDENT;i++)
-		grade[i] = (i+1)*10;
+	{
+		scanf("%d", &grade[i]);
+		sum += grade[i];
+	}
 	
-	for (i=0;i<N_STUDENT;i++)
-		printf("grade[%i] = %i\n", i, grade[i]);
+	average = sum / N_STUDENT;
+	printf("score average : %i\n", average);
 		
 	system("PAUSE");
 	return 0;
